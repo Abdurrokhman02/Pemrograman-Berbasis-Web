24b1153ee73883f5eb0745a6a71df3b3cd541a55
Abdurrokhman <148690309+Abdurrokhman02@users.noreply.github.com>
2025-05-09 15:55:57 +0700

    praktikum

---
+++ Praktikum/Abdurrokhman_060_Praktikum 6/phpraktikum.php
@@ -0,0 +1,12 @@
+<?php
+    define("nama", "Abdurrokhman");
+    $umur = 19;
+    $matkul = ["EIS", "PBW", "OS"];
+
+    echo "hallo, selamat datang di dunia PHP <br>";
+    echo "Nama: " . nama . "<br>";
+    echo "Umur: " . $umur . " tahun <br>";
+    
+    echo "My favorite matkul gw is " . $matkul[1] . "<br>";
+    echo "Tahun depan aku berumur " . $umur+1;
+?>
